f375f4159165a33fbef0704f09acf924ee46532c
tavo1987 <tavo198718@gmail.com>
2017-04-26 11:10:19 -0500

    Added event bus file

---
+++ src/EventBus.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+
+// Create a global Event Bus
+let EventBus = new Vue()
+
+// Add to Vue properties by exposing a getter for $bus
+Object.defineProperties(Vue.prototype, {
+    $bus: {
+        get: function () {
+            return EventBus
+        }
+    }
+});
