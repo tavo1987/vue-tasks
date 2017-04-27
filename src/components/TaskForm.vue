1023c3903f2940de91530aa474225a923e5319ea
tavo1987 <tavo198718@gmail.com>
2017-04-26 20:42:25 -0500

    Added key up event to task form component

--- src/components/TaskForm.vue
+++ src/components/TaskForm.vue
@@ -1,7 +1,7 @@
 <template>
     <form @submit.prevent="submit" class="columns">
         <div class="column is-8">
-            <input v-model="newTask" type="text" class="input">
+            <input v-model="newTask" type="text" class="input" @keyUp.enter="submit">
         </div>
         <div class="column">
             <button :disabled="this.newTask == ''" class="button is-primary" type="submit">Add task</button>
